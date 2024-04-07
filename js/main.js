563e90f03af38b512889f561865ea6b391670239
Tregubov Maksim <tregubov-m@inbox.ru>
2024-04-07 11:10:51 +0300

    Add slider functionality and styles for header and reviews sections
    - Added slider functionality and styles for header and reviews sections.
    - Included new SVG files for slider buttons.
    - Updated HTML and JavaScript files to implement Swiper sliders.
    - Created SCSS files for slider styles in header and reviews sections.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-function slider(mySlider, sliderValue){
-  valPercent = (mySlider.value / mySlider.max)*100;
+function slider(mySlider, sliderValue) {
+  valPercent = (mySlider.value / mySlider.max) * 100;
   mySlider.style.background = `linear-gradient(to right, #3264fe ${valPercent}%, #d5d5d5 ${valPercent}%)`;
   sliderValue.textContent = mySlider.value;
 }
@@ -15,3 +15,31 @@ slider(mySlider_2, sliderValue_2);
 const mySlider_3 = document.getElementById("my-slider-3");
 const sliderValue_3 = document.getElementById("slider-value-3");
 slider(mySlider_3, sliderValue_3);
+
+var swiper = new Swiper(".header__slider", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 4000,
+  },
+  navigation: {
+    nextEl: ".header-button-next",
+    prevEl: ".header-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper_1 = new Swiper(".reviews__slider", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  slidesPerView: 3,
+  navigation: {
+    nextEl: ".reviews-button-next",
+    prevEl: ".reviews-button-prev",
+  },
+});
